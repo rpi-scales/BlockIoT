7c6aa34d10203a881cde2d8467baf5d95d600c29
MananShukla <43486501+MananShukla@users.noreply.github.com>
2021-05-26 15:52:15 -0400

    Add files via upload

--- new_BlockIoT/sms.py
+++ new_BlockIoT/sms.py
@@ -3,11 +3,12 @@ carriers = ['@mms.att.net','@tmomail.net','@vtext.com','@page.nextel.com']
 
 def send(message,contract):
 	print("Please add credentials!")
+	return
 	config = ast.literal_eval(contract.functions.get_config_file().call())
 	# Replace the number with your own, or consider using an argument\dict for multiple people.
 	for carrier in carriers:
 		to_number = config["communication"]["phone"] + carrier
-		auth = ('manan.shukla2001@gmail.com', 'shivtandav432')
+		auth = ('EMAIL', 'PASS')
 
 		# Establish a secure session with gmail's outgoing SMTP server using your gmail account
 		server = smtplib.SMTP( "smtp.gmail.com", 587 )
